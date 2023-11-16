e50dbf0ac59f595d53c52d34e1ff04135485a38e
DMNerd <7889445+DMNerd@users.noreply.github.com>
2023-11-16 23:01:45 +0100

    Revert "Do not bind unneeded"
    This reverts commit f4c97be0a07f4a4ead005a93b9c75f92c24788e2.

--- Resources/VFIO Hooks/macOS/unbind_vfio.sh
+++ Resources/VFIO Hooks/macOS/unbind_vfio.sh
@@ -12,5 +12,9 @@ modprobe -r vfio
 ## Unbind gpu from vfio and bind to radeon
 virsh nodedev-reattach $PASS_GPU_AMD
 virsh nodedev-reattach $PASS_GPU_AMD_AUD
+## Unbind ssd from vfio and bind to nvme
+virsh nodedev-reattach $PASS_SSD1
+## Unbind WIFI and attach back
+virsh nodedev-reattach $PASS_WIFI
 
 modprobe amdgpu
